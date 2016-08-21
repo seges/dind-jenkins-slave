fb9b45d341368ff263015ffe393ac04c5b4dd0da
Ladislav Gazo <gazo@seges.sk>
2016-08-21 17:05:00 +0200

    jenkins slave init script and SSH ignore host keys

---
+++ jenkins-slave/jenkins-slave-init.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ "$JENKINS_SLAVE_SSH_NO_STRICT_CHECK" == "true" ]; then
+  mkdir $HOME/.ssh
+  cp /opt/jenkins-slave/ssh/config $HOME/.ssh/
+fi
+
+java -jar swarm-client-2.0-jar-with-dependencies.jar -master http://$MASTER_PORT_8080_TCP_ADDR:$MASTER_PORT_8080_TCP_PORT $EXTRA_PARAMS
